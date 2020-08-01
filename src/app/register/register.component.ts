cb5219765de08f34a6eb0c9133ad0a2b303c9c3b
Onoyama Rikuo <evang00dd@gmail.com>
2020-08-01 20:17:25 +0900

    新規登録処理

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {NotificationType} from '../enum/notification-type.enum';
+import {HttpErrorResponse} from '@angular/common/http';
+import {User} from '../model/user';
+import {NotificationService} from '../service/notification.service';
+import {AuthenticationService} from '../service/authentication.service';
+import {Router} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +13,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  public showLoading: boolean;
+  private subscriptions: Subscription[] = [];
 
-  constructor() { }
+  constructor(private router: Router, private authenticationService: AuthenticationService,
+              private notificationService: NotificationService) {}
 
   ngOnInit(): void {
+    if (this.authenticationService.isUserLoggedIn()) {
+      this.router.navigateByUrl('/user/management');
+    }
   }
 
+  public onRegister(user: User): void {
+    this.showLoading = true;
+    this.subscriptions.push(
+      this.authenticationService.register(user).subscribe(
+        (response: User) => {
+          this.showLoading = false;
+          this.sendNotification(NotificationType.SUCCESS, `A new account was created for ${response.firstName}.
+          Please check your email for password to log in.`);
+        },
+        (errorResponse: HttpErrorResponse) => {
+          this.sendNotification(NotificationType.ERROR, errorResponse.error.message);
+          this.showLoading = false;
+        }
+      )
+    );
+  }
+
+  private sendNotification(notificationType: NotificationType, message: string): void {
+    if (message) {
+      this.notificationService.notify(notificationType, message);
+    } else {
+      this.notificationService.notify(notificationType, 'An error occurred. Please try again.');
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
 }
